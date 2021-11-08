ae5b8d8c6e904921e318e42bdadcae7f62886cd2
banksjazz87 <banksjazz87@gmail.com>
2021-11-08 14:29:57 -0500

    I have created a server that displays the build folder after you run a new build

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 4500;
-const path = require('path');
+//const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use('/', express.static('build'));
 
 //app.use('/static', express.static(path.join(__dirname, 'public')));
 
