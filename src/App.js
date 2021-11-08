2fcf82912ea39a73a6fb52719dd255bdafd40697
banksjazz87 <banksjazz87@gmail.com>
2021-11-08 05:38:42 -0500

    chore: I removed a few of the files that I don't need, and created a server file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code> src / App.js </code> and save to reload yeah, oh yeah yeah.{" "}
-        </p>{" "}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React{" "}
-        </a>{" "}
-      </header>{" "}
-    </div>
+   <div>
+       <h1>Hello Mother Trucking World</h1>
+       <Text />
+   </div>
   );
+}
+
+function Text() {
+    return(
+        <div>
+            <p>This is just a test</p>
+        </div>
+    );
 }
 
 export default App;
