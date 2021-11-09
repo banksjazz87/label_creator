e3c13ab5d20820df67a287892ac06295a901894e
banksjazz87 <banksjazz87@gmail.com>
2021-11-09 05:50:52 -0500

    chore: I have set up the react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
    <div>
-       <h1>Hello Mother Trucking World</h1>
+       <h1>Welcom to React Router!</h1>
+       <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='about' element={<About />} />
+       </Routes>
        <Text />
    </div>
   );
@@ -16,5 +22,37 @@ function Text() {
         </div>
     );
 }
+
+function Home() {
+    return(
+        <div>
+          <main>
+            <h1>This is the home Page</h1>
+            <p>Welcome to the home page</p>
+          </main>
+          <nav>
+              <Link to='/about'>About</Link>
+          </nav>
+
+        </div>
+    )
+}
+
+function About() {
+    return(
+        <div>
+          <main>
+            <h1>This is the about page</h1>
+            <p>Welcome to the about page</p>
+          </main>
+          <nav>
+              <Link to='/'>Home</Link>
+          </nav>
+
+        </div>
+    )
+}
+
+
 
 export default App;
