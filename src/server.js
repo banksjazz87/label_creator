e833b698e8dc66dff559a29dd4ed15da3331be13
banksjazz87 <banksjazz87@gmail.com>
2021-12-15 05:19:19 -0500

    chore: Cors has been successfully added

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 4500;
 //const path = require('path');
 
+app.use(cors());
 app.use('/', express.static('build'));
 
 app.listen(port, () => {
