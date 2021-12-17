44c06c8d7b543aaa97626843fb19ffb7e34a3622
banksjazz87 <banksjazz87@gmail.com>
2021-12-17 06:01:06 -0500

    chore: I am working typing up some dummy data to use for development

--- src/parents/packSlip.js
+++ src/parents/packSlip.js
@@ -1,6 +1,38 @@
 import * as React from "react";
 import "../App.css";
 
+
+//This is going to be dummy data used for development
+let userDataFromCreator = [{
+  shipFrom: {
+    company: "Seneca Printing Express",
+    street: "191 Howard Street",
+    city: "Franklin", 
+    state: "PA",
+    zip: "16323", 
+  },
+
+  shipTo: {
+    company: "Chris Banks",
+    street: "402 Myrtle Street",
+    city: "Emlenton", 
+    state: "PA",
+    zip: "16232", 
+    attention: "Chris"
+  }, 
+
+  skid: [{
+    itemDescription: "Giorgio Whole Whites 16oz",
+    qtyNeeded: "16,000", 
+    qtyShipped: "18,000", 
+    packsRolls: "1,000", 
+    qtyPerCarton: "16,000",
+    numOfCartons: "2",
+  }],
+
+  PO: "54791", 
+  Job: "176592"
+}]
 //async function using fetch to retrieve the data from the server
 const serverCall = async () => {
   const response = await fetch(
@@ -9,40 +41,42 @@ const serverCall = async () => {
 
   try {
     let updatedRes = await response.json();
+    //userDataFromCreator = updatedRes;
     console.log(updatedRes);
   } catch (e) {
     console.log("error", e);
   }
 };
 
-function TestText() {
-  return (
-    <div>
-      <h1>This is the pack slip page</h1>
-    </div>
-  );
-}
+
 
 class ParentPackSlip extends React.Component {
   constructor(props) {
     super(props);
-
+    
+    //Make This active before production
+    //serverCall();
+    
     this.state = {
-      test: "",
+      userData: userDataFromCreator[0]
     };
+    console.log(this.state);
   }
-  componentDidMount() {
-    //async function using fetch to retrieve the data from the server
-    serverCall();
-  }
+
+
   render() {
     return (
       <div>
-        <TestText />
-        <button type='button' onClick={serverCall}>Click for Data</button>
+        <h1>Packing Slip</h1>
       </div>
     );
   }
 }
+
+/*const Address = (data) => {
+  return(
+
+  )
+}*/
 
 export default ParentPackSlip;
