cd6fb2ec76e5a1218be3ed5e44ab0e7f20adb425
Chris <banksjazz87@gmail.com>
2022-01-01 09:38:22 -0500

    chore: I have all of the content being displayed correctly on the shipTo page

--- src/parents/shipTo.js
+++ src/parents/shipTo.js
@@ -97,12 +97,43 @@ class ParentShipTo extends React.Component {
   render() {
     return( 
     <div>
-    <h1> This file has been updated </h1>
+
+      <ShipToFrom 
+        toFrom="ship_from"
+        items={this.state.userData.shipFrom}
+      />
+
+      <ShipToFrom
+        toFrom="ship_to"
+        items={this.state.userData.shipTo}
+      />
+
+      <p style={{fontSize: '32px'}}>{`PO#: ${this.state.userData.PO}`}</p>
+
+      
     </div>
     )
   }
 }
 
+const ShipToFrom = (props) => {
+  const checkForItem = () => {
+    if(props.items.attention){
+      return(
+        <p>{`ATTENTION: ${props.items.attention}`}</p>
+      )
+    }
+  }
+
+  return (
+    <div id={props.ship_from}>
+      <p>{props.items.company}</p>
+      <p>{props.items.street}</p>
+      <p>{`${props.items.city}, ${props.items.state} ${props.items.zip}`}</p>
+      {checkForItem('attention')}
+    </div>
+  )
+}
 
 
 export default ParentShipTo;
