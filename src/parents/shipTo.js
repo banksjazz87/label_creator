9f2623bb410f2d4c59840b9645b6fb41399fcd7d
Chris <banksjazz87@gmail.com>
2022-01-02 21:48:57 -0500

    chore: I have created a file to store all of the dummy data for devolopment

--- src/parents/shipTo.js
+++ src/parents/shipTo.js
@@ -1,68 +1,6 @@
 import * as React from "react";
 import "../App.css";
-
-//This is going to be dummy data used for DEVELOPMENT
-//let userDataFromCreator;
-let userDataFromCreator = [
-  {
-    shipFrom: {
-      company: "Seneca Printing Express",
-      street: "191 Howard Street",
-      city: "Franklin",
-      state: "PA",
-      zip: "16323",
-      phone: "814-671-2074",
-    },
-
-    shipTo: {
-      company: "Chris Banks",
-      street: "402 Myrtle Street",
-      city: "Emlenton",
-      state: "PA",
-      zip: "16232",
-      attention: "Chris",
-    },
-
-    skid: [
-      {
-        itemDescription: "Giorgio Whole Whites 16oz",
-        qtyNeeded: "16,000",
-        qtyShipped: "18,000",
-        packsRolls: "1,000",
-        qtyPerCarton: "16,000",
-        numOfCartons: "2",
-      },
-      {
-        itemDescription: "Giorgio Chopped Pieces 16oz",
-        qtyNeeded: "32,000",
-        qtyShipped: "36,000",
-        packsRolls: "1,000",
-        qtyPerCarton: "16,000",
-        numOfCartons: "3",
-      },
-      {
-        itemDescription: "Phillips Guans 16oz",
-        qtyNeeded: "48,000",
-        qtyShipped: "48,000",
-        packsRolls: "1,000",
-        qtyPerCarton: "16,000",
-        numOfCartons: "3",
-      },
-      {
-        itemDescription: "Giorgio Stems 16oz",
-        qtyNeeded: "56,000",
-        qtyShipped: "64,000",
-        packsRolls: "1,000",
-        qtyPerCarton: "16,000",
-        numOfCartons: "4",
-      },
-    ],
-
-    PO: "54791",
-    Job: "176592",
-    date: "12/28/2021",
-  },
-];
+import userDataFromCreator from "../variables/dummyData"
 
 //Used in production mode
 //async function using fetch to retrieve the data from the server
