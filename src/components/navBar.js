89bb4af6567b573a56548902ab98601f2c56c229
Chris <banksjazz87@gmail.com>
2022-01-10 18:41:31 -0500

    chore: I am working on creating a hover state over the link options

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -9,16 +9,27 @@ const linkStyles = {
     textShadow: '.5px 1px #494949'
 }
 
+const hoverLinkStyles = {
+    textDecoration: 'none', 
+    fontSize: '1.5em', 
+    color: 'black',
+    textShadow: '.5px 1px #494949'
+ }
+
 class Nav extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       display: false,
-      width: 0
+      width: 0, 
+      over: false,
+      color: 'white'
     }
 
     this.showHide = this.showHide.bind(this);
     this.displayMenu = this.displayMenu.bind(this);
+    this.mouseOverOption = this.mouseOverOption.bind(this);
+    this.mouseLeaveOption = this.mouseLeaveOption.bind(this);
     
   }
 
@@ -66,10 +77,23 @@ class Nav extends React.Component {
     this.displayMenu();
   }
 
+  //Mouse over option function, this will change the state of the boolean labeled as 'option'
+  mouseOverOption = (e) => {
+      e.preventDefault();
+      e.target.setAttribute('style', "textDecoration: none; font-size: 1.5em; color: black; textShadow: .5pz 1px #494949");
+  }
+
+  mouseLeaveOption = (e) => {
+      e.preventDefault();
+      e.target.setAttribute('style', "textDecoration: none; font-size: 1.5em; color: white; textShadow: .5pz 1px #494949");
+  }
+
   render(){
   return (
     <div>
-    <MenuButton clickHandler={this.showHide}/>
+    <MenuButton clickHandler={this.showHide}
+                clicked={this.state.display}
+    />
     <nav id="navbar" 
         style={this.state.display ? 
         {display:"flex", height: "100vh", width:`${this.state.width}vw`} 
@@ -77,10 +101,35 @@ class Nav extends React.Component {
         {display: "flex", height: "100vh", width: `${this.state.width}vw`} 
         : {display: "none"}}>
 
-      <Link to="/shipping_creator" style={linkStyles}>Shipping Create Docs</Link>
-      <Link to="/pack_slip" style={linkStyles}>Pack Slip</Link>
-      <Link to="/labels" style={linkStyles}>Lables</Link>
-      <Link to="/ship_to_papers" style={linkStyles}>Ship To Slip</Link>
+     
+      <Link to="/shipping_creator" 
+        style={linkStyles} 
+        onMouseOver={this.mouseOverOption}
+        onMouseLeave={this.mouseLeaveOption}>
+        Shipping Create Docs
+      </Link>
+
+      <Link to="/pack_slip" 
+        style={linkStyles} 
+        onMouseOver={this.mouseOverOption}
+        onMouseLeave={this.mouseLeaveOption}>
+        Pack Slip
+     </Link>
+
+      <Link to="/labels" 
+        style={linkStyles} 
+        onMouseOver={this.mouseOverOption}
+        onMouseLeave={this.mouseLeaveOption}>
+        Lables
+      </Link>
+
+      <Link to="/ship_to_papers" 
+        style={linkStyles} 
+        onMouseOver={this.mouseOverOption}
+        onMouseLeave={this.mouseLeaveOption}>
+        Ship To Slip
+      </Link>
+
     </nav>
     </div>
   )};
@@ -88,7 +137,9 @@ class Nav extends React.Component {
 
 const MenuButton = (props) => {
     return(
-        <div id="hamburger_icon" onClick={props.clickHandler}>
+        <div id="hamburger_icon" 
+            onClick={props.clickHandler}
+            style={{backgroundColor: props.clicked ? "#494949" : "#082b44"}}>
             <div className="line"></div>
             <div className="line"></div>
             <div className="line"></div>
