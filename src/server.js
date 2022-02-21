9af02c53b74a2e437a415e6e6401c0bb5e6f5436
Chris <banksjazz87@gmail.com>
2022-02-21 05:48:59 -0500

    chore: I am working on the back end, by creating some basic CRUD operations with the mongo database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 4500;
+const MongoClient = require("mongodb").MongoClient;
+
 //const path = require('path');
 
 
@@ -14,6 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', express.static('build'));
 
+//all information pertaining to connecting to Mongo
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
 let currentData = {};
 let allData = [];
 
@@ -21,6 +27,12 @@ let allData = [];
 app.post('/shipping_creator/data', (req, res) => {
     currentData = req.body;
     allData.unshift(currentData);
+
+    const result = async() => await client.insertOne(currentData);
+    
+    result();
+
+    console.log(`A document was inserted with the _id: ${result.insertId}`);
 
     console.log(allData);
 
