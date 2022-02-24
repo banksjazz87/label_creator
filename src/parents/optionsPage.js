b98cff7debf4f5b472c79c58a105a1f69c4d4d45
Chris <banksjazz87@gmail.com>
2022-02-23 21:36:16 -0500

    chore: I have created a few ways to search for a specific document on the options page

--- src/parents/optionsPage.js
+++ src/parents/optionsPage.js
@@ -1,16 +1,21 @@
 import * as React from 'react'
 import "../assets/options.scss"
 import "../assets/library.scss"
+
+const searchSelections = ["Select One", "Company", "Job#", "PO#", "Date"];
+
 class OptionsPage extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
             search: false,
-            new: false
+            new: false,
+            searchBy: ""
         }
 
         this.choiceClick = this.choiceClick.bind(this);
+        this.optionSelection = this.optionSelection.bind(this);
     }
 
     choiceClick(e){
@@ -20,13 +25,24 @@ class OptionsPage extends React.Component{
 
         if(currentId === 'search'){
             this.setState({
-                search: true
+                search: true,
+                new: false
             })
         }else{
             this.setState({
+                search: false,
                 new: true
             })
         }
+    }
+
+    optionSelection(e){
+
+        e.preventDefault();
+
+        this.setState({
+            searchBy: e.target.value
+        })
     }
 
     render() {
@@ -46,8 +62,15 @@ class OptionsPage extends React.Component{
                         label="Search" 
                         className="button"
                 />
-             </div>
+            </div>
 
+                <Options searching={this.state.search}
+                         changeHandler={this.optionSelection}
+                />
+                <Input searchType={this.state.searchBy}
+                       searching={this.state.search}
+                />
+             
             </div>
         )
     }
@@ -55,7 +78,39 @@ class OptionsPage extends React.Component{
 
 const Choice = (props) => {
     return(
-    <button id={props.idName} type="button">{props.label}</button>
+    <button id={props.idName}                   
+            type="button"
+            onClick={props.clickHandler}
+    >
+        {props.label}
+    </button>
+    )
+}
+
+const Options = (props) => {
+    const allOptions = searchSelections.map((x, y) => {
+        return(
+        <option id={x} 
+                key={x + "_y"}
+                >
+        {x}
+        </option>
+    )
+    })
+    return(
+        <select style={props.searching ? {display: "flex"} : {display: "none"}}
+        onChange={props.changeHandler}>
+            {allOptions}
+        </select>
+    )
+}
+
+const Input = (props) => {
+    return(
+        <form style={props.searchType.length === 0 ? {display: "none"} : {display: "flex"}}>
+        <label>{props.searchType}</label>
+        <input type="text"></input>
+        </form>
     )
 }
 
