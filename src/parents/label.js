8fcf66bb58dd1d2c4711722ef7a240f2bfa47956
Chris <banksjazz87@gmail.com>
2022-04-28 21:22:10 -0400

    chore: I have fixed a few of the responsive issues I was encountering with the lables page

--- src/parents/label.js
+++ src/parents/label.js
@@ -1,13 +1,12 @@
 import * as React from "react";
 import "../assets/label.scss";
-import "../assets/nav.scss"
+import "../assets/nav.scss";
 
 //userDataFromCreator used for development mode
 //import userDataFromCreator from "../variables/dummyData";
 import PrintButton from "../components/printButton";
-import serverCall from "../functions/serverCall"
-import changeDateFormat from "../functions/dateFormat.js"
-
+import serverCall from "../functions/serverCall";
+import changeDateFormat from "../functions/dateFormat.js";
 
 class ParentLabels extends React.Component {
   constructor(props) {
@@ -18,7 +17,7 @@ class ParentLabels extends React.Component {
       fetched: false,
       //switch userData to userDataFromCreator[0] for development and "" for production
       userData: "",
-      count: 0
+      count: 0,
     };
 
     this.incrementHandler = this.incrementHandler.bind(this);
@@ -26,107 +25,111 @@ class ParentLabels extends React.Component {
   }
 
   //use this function only for production
- componentDidMount(){
-    serverCall("/allData")
-    .then(items => this.setState({
+  componentDidMount() {
+    serverCall("/allData").then((items) =>
+      this.setState({
         fetched: true,
-        userData: items[0]
-      }))
+        userData: items[0],
+      })
+    );
   }
 
   incrementHandler = (e) => {
     e.preventDefault();
 
-    if(this.state.count === this.state.userData.skid.length - 1){
-      alert('All of the labels have been printed.')
-  }else{
-    this.setState((prevCount) => ({
-      count: prevCount.count + 1
-    }))
-  }
-  }
+    if (this.state.count === this.state.userData.skid.length - 1) {
+      alert("All of the labels have been printed.");
+    } else {
+      this.setState((prevCount) => ({
+        count: prevCount.count + 1,
+      }));
+    }
+  };
 
   decrementHandler = (e) => {
     e.preventDefault();
 
-    if(this.state.count > 0){
+    if (this.state.count > 0) {
       this.setState((prevCount) => ({
-        count: prevCount.count -1
-      }))
-    }else{
-      alert('You are currently on the first label');
+        count: prevCount.count - 1,
+      }));
+    } else {
+      alert("You are currently on the first label");
+    }
+  };
+  render() {
+    if (this.state.fetched) {
+      return (
+        <div>
+          <p id="labels_needed">{`Print ${
+            this.state.userData.skid[this.state.count].numOfCartons
+          }`}</p>
+
+          <div id="label_container">
+            <TopHeading
+              job={this.state.userData.Job}
+              shipFrom={this.state.userData.shipFrom.company}
+            />
+
+            <LabelMiddle
+              description={
+                this.state.userData.skid[this.state.count].itemDescription
+              }
+            />
+
+            <LabelBottom
+              attention={this.state.userData.shipTo.attention}
+              purchaseOrder={this.state.userData.PO}
+              packs={this.state.userData.skid[this.state.count].packsRolls}
+              quantityPerCarton={
+                this.state.userData.skid[this.state.count].qtyPerCarton
+              }
+              date={changeDateFormat(this.state.userData.date)}
+            />
+          </div>
+
+          <div id="label_buttons_container">
+            <Button
+              id="previous_button"
+              clickHandler={this.decrementHandler}
+              text="Previous"
+            />
+
+            <PrintButton />
+
+            <Button
+              id="next_button"
+              clickHandler={this.incrementHandler}
+              text="Next"
+            />
+          </div>
+        </div>
+      );
+    } else {
+      return <h1>Fetching</h1>;
     }
   }
-  render() {
-    if(this.state.fetched){
-    return (
-      <div>
-        <p id="labels_needed">{`Print ${this.state.userData.skid[this.state.count].numOfCartons}`}</p>
-      
-
-      <div id="label_container">
-        <TopHeading 
-          job={this.state.userData.Job}
-          shipFrom={this.state.userData.shipFrom.company}
-        />
-
-        <LabelMiddle
-          description={this.state.userData.skid[this.state.count].itemDescription}
-        />
-       
-        <LabelBottom
-          attention={this.state.userData.shipTo.attention}
-          purchaseOrder={this.state.userData.PO}
-          packs={this.state.userData.skid[this.state.count].packsRolls}
-          quantityPerCarton={this.state.userData.skid[this.state.count].qtyPerCarton}
-          date={changeDateFormat(this.state.userData.date)}
-        />
-      </div>
-
-      <div id="label_buttons_container">
-        <Button
-          id="previous_button"
-          clickHandler={this.decrementHandler}
-          text="Previous"
-        />
-
-       <PrintButton />
-
-        <Button 
-          id="next_button"
-          clickHandler={this.incrementHandler}
-          text="Next"
-        />
-      </div>
-      </div>
-    );
-  }else{
-    return(
-      <h1>Fetching</h1>
-    )
-  }
-}
 }
 
 const TopHeading = (props) => {
-  return(
+  return (
     <div id="top_of_label" class="label_content">
       <p id="job">{`Job# ${props.job}`}</p>
       <p id="company_name">{props.shipFrom}</p>
     </div>
-  )
-}
+  );
+};
 
 const LabelMiddle = (props) => {
-  return(
+  return (
     <div id="middle_label" class="label_content">
       <p id="description">{props.description}</p>
     </div>
-  )
-}
+  );
+};
 
 const LabelBottom = (props) => {
-  return(
+  return (
     <div id="bottom_label" class="label_content">
       <p id="attention">{`Attention: ${props.attention}`}</p>
       <p id="purchase_order">{`PO#: ${props.purchaseOrder}`}</p>
@@ -134,13 +137,15 @@ const LabelBottom = (props) => {
       <p id="qtyPerCarton">{`Quantity= ${props.quantityPerCarton}`}</p>
       <p id="date">{`Date: ${props.date}`}</p>
     </div>
-  )
-}
+  );
+};
 
 const Button = (props) => {
-  return(
-    <button className="label_buttons" onClick={props.clickHandler}>{props.text}</button>
-  )
-}
+  return (
+    <button className="label_buttons" onClick={props.clickHandler}>
+      {props.text}
+    </button>
+  );
+};
 
 export default ParentLabels;
