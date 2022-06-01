89d65ba3eae9bd50dfd85cce1794a0b7f0c15254
Chris <banksjazz87@gmail.com>
2022-06-01 05:55:02 -0400

    chore: I added a count feature on the label page, so that the user knows their current progress

--- src/parents/label.js
+++ src/parents/label.js
@@ -4,7 +4,7 @@ import "../assets/nav.scss";
 import TextEdit from "../components/textEdit.js";
 
 //userDataFromCreator used for development mode
-//import userDataFromCreator from "../variables/dummyData";
+import userDataFromCreator from "../variables/dummyData";
 
 import PrintButton from "../components/printButton";
 import serverCall from "../functions/serverCall";
@@ -16,9 +16,9 @@ class ParentLabels extends React.Component {
 
     this.state = {
       //switch fetched to true for development, false for production
-      fetched: false,
+      fetched: true,
       //switch userData to userDataFromCreator[0] for development and "" for production
-      userData: "",
+      userData: userDataFromCreator[0],
       count: 0,
       showEditBox: false,
     };
@@ -35,14 +35,14 @@ class ParentLabels extends React.Component {
   }
 
   //use this function only for production
-  componentDidMount() {
+  /*componentDidMount() {
     serverCall("/allData").then((items) =>
       this.setState({
         fetched: true,
         userData: items[0],
       })
     );
-  }
+  }*/
 
   incrementHandler = (e) => {
     e.preventDefault();
@@ -151,6 +151,9 @@ class ParentLabels extends React.Component {
             />
           </div>
 
+          <p id="progress">{`${this.state.count + 1}/${
+            this.state.userData.skid.length
+          }`}</p>
           <div id="label_buttons_container">
             <Button
               id="previous_button"
